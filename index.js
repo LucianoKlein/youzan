974658e8565c383e31f7b232dec6d50f06d94483
LucianoKlein <kleinersilver@gmail.com>
2018-02-16 07:53:48 +0800

    引入了vue

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-let url = 'http://rapapi.org/mockjsdata/31797/index/hotLists';
+// let url = 'http://rapapi.org/mockjsdata/31797/index/hotLists';
 
-let axios = require("axios")
+// let axios = require("axios")
 
-axios.post(url, {
-    pageNum: 1,
-    pageSize: 10
-}).then(res => {
-    console.dir(res.data.lists);
-});
+// axios.post(url, {
+//     pageNum: 1,
+//     pageSize: 10
+// }).then(res => {
+//     console.dir(res.data.lists);
+// });
 
+
+let Mock = require("mockjs");
+console.log(Mock.mock({
+    "number|1-100.2": 1
+}))
